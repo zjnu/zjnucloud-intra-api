a8b9f03bf8448a61f02840995514b3b1ca247a2d
ddMax <logumb@gmail.com>
2016-06-09 01:37:14 +0800

    Adjust emis urls regex

--- emis/urls.py
+++ emis/urls.py
@@ -4,10 +4,10 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from emis.views import ScoreList, CourseTableList, ExamScheduleList
 
 urlpatterns = [
-    url(r'^score/', ScoreList.as_view(), name='score'),
+    url(r'^score/$', ScoreList.as_view(), name='score'),
     url(r'^course/$', CourseTableList.as_view(), name='course'),
     url(r'^course/(\d+)/(\d+)/$', CourseTableList.as_view(), name='course'),
-    url(r'^exam/', ExamScheduleList.as_view(), name='exam'),
+    url(r'^exam/$', ExamScheduleList.as_view(), name='exam'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
